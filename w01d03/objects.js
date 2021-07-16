abb7eecb4a1981154e18534c4a9b1687864a78dc
James Bain <james.bain@lighthouselabs.com>
2021-07-15 22:31:29 -0600

    partition out objects.js and object_keys.js into their own concepts;

--- w01d03/objects.js
+++ w01d03/objects.js
@@ -1,54 +1,5 @@
-// example object with an object literal
-const obj = {
-  name: 'etta', 
-  age: 77, 
-  canTieShoes: true
-};
-
-// accessing object values
-// square bracket notation
-console.log("obj['name'] -->", obj['name']);
-// dot notation
-console.log("obj.name --> ", obj.name);
-
-// variable to access value
-const key = 'canTieShoes';
-console.log("obj[key]", obj[key])
-console.log("obj.key", obj.key) // --> undefined
-
-
-// primitives are immutable 
-// they can't be changed... just reassigned
-const creature = 'bigfoot';
-
-// creature a function that takes in a creature as an input
-// changes creature to 'el chupacabra'
-// prints out the creature to the console
-const printDifferentCreature = function(creature){
-  creature = 'el chupacabra';
-  console.log('creature during function:', creature)
-}
-
-// what do you think the variable `creature` will be after it is called?
-console.log('creature before function:', creature);
-printDifferentCreature(creature);
-console.log('creature after function', creature)
-
-const creatureObject = {
-  name: 'bigfoot',
-  isTall: true 
-};
-
-const printDifferentCreatureName = function(obj){
-  obj.name = 'el chupacabra';
-  console.log('creature name during function:', obj.name);
-}
-
-
-console.log('creature name before function:', creatureObject);
-printDifferentCreatureName(creatureObject);
-console.log('creature name after function', creatureObject);
-
+// why should we use objects?
+// create an entity where values have a cohesive relationship
 
 const creatureOne = 'bigfoot';
 const nameOne = 'bigfoot';
@@ -58,11 +9,19 @@ const creatureTwo = 'el chupacabra';
 const nameTwo = 'billie';
 const locationTwo = 'Puerto Rico';
 
+// ..creatureThree...creatureFour...creatureSixMillionThreeThousandTwentyFour 
 
+// perhaps I could  make arrays for conceptually similar information
 const creatures = ['bigfoot', 'el chupacabra', 'loch ness monster', 'unicorn'];
-const names = ['harry', 'billie', 'lauren', 'alex'];
+const names = ['harry', 'billie', 'lauren', 'alex']; 
 const locations = ['U.S.', 'Puerto Rico', 'Scottland', 'Mongolia'];
 
+// perhaps arrays of an entity?
+// is this problematic
+const bigfoot = ['bigfoot', 'harry', 'U.S.', true];
+const elChupacabra = ['el chupacabra', 'billie', 'Puerto Rico', false];
+
+// keys can hold semantic meaning that make our code easier to understand
 const mythicalCreature = {
   type: 'bigfoot',
   name: 'harry',
