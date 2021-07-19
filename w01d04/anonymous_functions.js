6668a6f6ed323fc67faa8651cd4c697772998359
James Bain <james.bain@lighthouselabs.com>
2021-07-19 17:57:59 -0600

    write a file for anonymous functions

---
+++ w01d04/anonymous_functions.js
@@ -0,0 +1,17 @@
+// so in the previous example we wrote a function
+// and passed it to the yellAtJames function
+const sayHello = function(name) {
+  return `Hello, ${name}`;
+}
+
+// the yellAtJames function takes in another function
+// in in the function body, calls the callback and passes
+// "James" as a parameter of the callback. The result of the
+// callback is then converted to uppercase in the higher-order function 
+// return
+const yellAtJames = function(anotherFunc) {
+  const statement = anotherFunc('James');
+  return `${statement.toUpperCase()}!!!`
+}
+
+// anonymous functions
