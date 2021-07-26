6e8f6977407e0c977ab34c8724508a6a6cc5b50f
James Bain <james.bain@lighthouselabs.com>
2021-07-26 10:07:16 -0600

    write section for blocking code

---
+++ w02d02/blocking.js
@@ -0,0 +1,30 @@
+// javascript is synchronous
+// single-threaded (on process executes at a time)
+
+// we all know what is going to happen here
+// console.log(1);
+// console.log(2);
+// console.log(3); 
+
+// what if we had an expensive operation
+const stop = 10000;
+console.log('(1) I run first');
+
+for(let i = 0; i < stop; i++){
+  const date = new Date();
+  console.log(date);
+}
+console.log('(2) I run last')
+
+// getTime here is a method to get the number of milleseconds that have 
+// elapsed since the Unix epoch (00:00:00 utc jan 1 1970)
+const startTime = new Date().getTime();
+
+for(let i = 0; i < stop; i++){
+  const date = new Date();
+  console.log(date);
+}
+
+const endTime = new Date().getTime();
+const elapsedTime = endTime - startTime;
+console.log(`The for loop took ${elapsedTime}ms to run`)
