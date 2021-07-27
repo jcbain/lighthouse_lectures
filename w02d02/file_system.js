9ee28b3efd1dc09919b19be8608a218ca6bab76c
James Bain <james.bain@lighthouselabs.com>
2021-07-26 20:35:29 -0600

    add file system section

---
+++ w02d02/file_system.js
@@ -0,0 +1,25 @@
+const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require('constants');
+const fs = require('fs');
+
+// const data = fs.readFileSync('./index.html')
+// will return a buffer hexidecimal encoding
+// buffer allow us to work with binary data
+// console.log(data)
+
+// encoding - how our 1 and 0s are lined up
+// utf-8 has lots of characters including emojis
+
+const data = fs.readFileSync('./index.html', {encoding: 'utf-8'})
+console.log(data)
+
+// readFile uses an error first callback
+// again it would have to be run after all of our sync code
+fs.readFile('./index.html',{encoding: 'utf-8'}, (err, data) => {
+  if(err) {
+    return console.log(err);
+  }
+
+  console.log(data.length)
+})
+
+// console.log('i should run first')
