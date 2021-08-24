fde48670947f192053634c604ff839a18daeb75d
James Bain <james.bain@lighthouselabs.com>
2021-08-23 23:08:51 -0600

    make a demo

--- w02d03/demo/client.js
+++ w02d03/demo/client.js
@@ -1,13 +1,38 @@
 const net = require('net');
 
-const client = net.createConnection({
-  port: 3001,
-  host: 'localhost'
-})
+// create a client that connects to a server
+// at a given ip and port
+const client = net.createConnection({ 
+  host: 'localhost',
+  port: 3001
+});
 
+// set the encoding for the incoming messages to the
+// client
+client.setEncoding('utf8')
+
+// when the client recieves data from the server
+// console log it
+client.on('data', (data) => {
+  console.log(data)
+});
+
+// react to the connection of the server
+// and within the connection react to
+// input standard input and write that back 
+// to the server
 client.on('connect', () => {
-  console.log('you have connected to the server')
   process.stdin.on('data', data => {
     client.write(data)
   })
 })
+
+// once disconnect, tell the client
+// they are disconnected and end the
+// process session
+client.on('end', () => {
+  console.log('disconnected from server');
+  process.exit()
+});
+
+
--- w02d03/demo/server.js
+++ w02d03/demo/server.js
@@ -1,16 +1,52 @@
 const net = require('net');
+const { generateRandomEmoji, generateRandomId } = require('../helpers');
+
+// a list of clients
+const listOfClients = [];
+
+// functions to write back to client what they are saying
+const writeBack = (data, client) => {
+  client.write(`🦜: ${data}`)
+}
+
+const writeBackToEveryone = (data, client, listOfClients) => {
+  for(const currentClient of listOfClients) {
+    if(currentClient.id !== client.id) {
+      currentClient.write(`\t${client.avatar}: ${data}`)
+    }
+  }
+}
 
 const server = net.createServer()
 
 server.on('connection', client => {
-  console.log('someone connected!')
-  client.setEncoding('utf8')
-  client.on('data', data => console.log(data))
-})
+  // add some attributes to deliniate clients
+  client.id = generateRandomId();
+  client.avatar = generateRandomEmoji();
 
-server.on('close',function(){
-  console.log('Server closed !');
-});
+  // add client to the list of clients
+  listOfClients.push(client)
+
+  // log to the server that a new client has connected
+  console.log(`\t${client.avatar} ${client.id} is connected!`)
+
+  // set the encoding
+  client.setEncoding('utf8')
+
+  // write to the client a welcome message
+  client.write('🦜 hello there from the server\n')
+  client.write(`🦜 your avatar is ${client.avatar} and your id is ${client.id}\n`)
+
+  // handle the event that a client sends data to the server
+  client.on('data', data => 
+    console.log(`${client.avatar} ${client.id}: ${data}`)
+  )
+  client.on('data', data => {
+    // broadcast
+    writeBackToEveryone(data, client, listOfClients)
+  })
+
+})
 
 server.listen(3001, function(){
   console.log('we are listening')
